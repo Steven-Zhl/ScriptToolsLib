6edd5ee1407641725eeefde5686c62f8e9b6edbb
Steven Zhl <Stevenzhl1001@gmail.com>
2022-08-31 12:50:39 +0800

    add PowerfulPixivDownloadHelper.py
    这个脚本是辅助Chrome插件“Powerful Pixiv Downloader”使用的，估计对大多数人没啥用。

---
+++ PowerfulPixivDownloadHelper.py
@@ -0,0 +1,98 @@
+from enum import Enum
+
+import openpyxl
+
+
+class Games(Enum):
+    战双帕弥什 = ["战双帕弥什", "战双", "Punishing", "Gray Raven", ]
+    鸣潮 = ["鸣潮", ]
+    原神 = ["原神", "Genshin Impact", "genshin", "Genshin", ]
+    明日方舟 = ["明日方舟", "Arknights", ]
+    碧蓝航线 = ["碧蓝航线", "AzurLane", "Azur", "azur", "AZUR", ]
+    无期迷途 = ["无期迷途", ]
+    崩坏3 = ["崩坏3", "Honkai", "崩壊3", "崩壊", ]
+    尼尔机械纪元 = ["Nier", "autometa", "Autometa", "nier", "NieR", ]
+    少女前线 = ["少女前线", "Frontline", "frontline", ]
+    绝区零 = ["绝区零", ]
+    幻塔 = ["幻塔", ]
+
+
+class Sheet:
+    """
+    被操作的Sheet
+    """
+
+    def __init__(self, sheet: openpyxl.Workbook.worksheets, tags_col=3, row_length=9):
+        """
+
+        :param sheet:被操作的sheet
+        :param tags_col: 划分游戏的内容所在列
+        :param row_length: 每行内容的长度
+        """
+        self.sheet = sheet  # 被操作的工作表
+        self.tags_col = tags_col  # 要检查的那一列
+        self.row_length = row_length  # 每一行的内容区间
+
+    def removeGameToNewXlsx(self, newXlsxPath: str, existTitleBar: bool, remain=False):
+        """
+        将当前（旧）文件中的内容根据游戏不同复制到新文件中
+
+        :param newXlsxPath: 新文件的绝对路径
+        :param existTitleBar: 是否存在标题栏
+        :param remain: 把图片信息复制到新文件中时，是否保留原本文件中的图片信息
+        """
+        game_name_list = [item.name for item in Games]  # 获取所有的游戏名称
+        # 为所有游戏创建Sheet，并初始化
+        wb_new = openpyxl.Workbook()
+        del wb_new["Sheet"]
+        for name in game_name_list:
+            wb_new.create_sheet(title=name)
+            if existTitleBar:
+                wb_new[name].append([cell.value for cell in tuple(self.sheet)[0]])
+
+        # 逐行检测游戏
+        for row_cells in (tuple(self.sheet) if not existTitleBar else tuple(self.sheet)[1:]):
+            origin_row = self.Row(row=row_cells, row_length=self.row_length)
+            for game in tuple(Games):
+                if origin_row.checkGame(tags_col=self.tags_col, gameTags=game.value):
+                    wb_new[game.name].append([cell.value for cell in origin_row.row])  # 将本行内容添加到新的文件中
+                    if not remain:
+                        origin_row.clear()
+        wb_new.save(newXlsxPath)
+
+    class Row:
+        def __init__(self, row: tuple, row_length):
+            self.row = row
+            self.row_length = row_length
+
+        def clear(self):
+            """
+            清理掉本行的内容
+            """
+            for item in self.row:
+                item.value = ""
+
+        def checkGame(self, tags_col, gameTags: list):
+            """
+            检查该行内容是否属于该游戏
+
+            :param tags_col: tag所在的col
+            :param gameTags: 用于硬匹配的tags
+            :return: bool
+            """
+            tagContent = self.row[tags_col - 1].value
+            for tag in gameTags:
+                if tag in tagContent:
+                    return True
+                else:
+                    continue
+            else:
+                return False
+
+
+wb = openpyxl.load_workbook(filename="这里填写原文件路径")
+ws = wb["这里填写被操作的sheet名称"]  # 如果只有一个sheet，这里可以改成ws = wb.active
+operate = Sheet(sheet=ws, tags_col=3, row_length=3)  # tags_col是检查tag的所在列；row_length是每行内容的长度
+operate.removeGameToNewXlsx(newXlsxPath="这里填写要保存的路径", existTitleBar=True,
+                            remain=True)  # existTitleBar:是否有标题栏；remain:在复制时是否保留原有的内容
+wb.save("这里填写原文件路径")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 脚本及小程序列表
 
-## M3U8_Decrypt.py
+## [M3U8_Decrypt.py](/M3U8_Decrypt.py)
 ### 介绍：
 * 使用Python.Crypto库进行文件的AES解密、合并
 ### 环境要求：
@@ -15,48 +15,7 @@
 ### 用法：
 * pass
 
-## 电池状态报告.bat
-### 介绍：
-* 使用cmd命令生成电池状态报告文件，并自动打开
-### 环境要求：
-* Windows
-* cmd
-* Powershell
-### 环境配置：
-* 无
-### 用法：
-* 双击直接运行
-* cmd环境下，直接键入文件路径
-* Powershell环境下，键入'cmd /c "[文件路径]"'
-
-## 《大学生创新创业》搜题工具.exe
-### 介绍：
-* 使用pyinstaller将解析题目的爬虫和搜索题目的Python源文件打包生成的文件
-### 环境要求：
-* Windows
-### 环境配置：
-* 无
-### 用法：
-* 需要提前将《大学生创新创业》的考试页面保存到本地
-* 双击直接运行
-* 按提示依次输入考试页面的地址并选择模式
-* 等待输出答案
-
-## 电池状态报告.bat
-### 介绍：
-* 使用cmd命令生成电池状态报告文件，并自动打开
-### 环境要求：
-* Windows
-* cmd
-* Powershell
-### 环境配置：
-* 无
-### 用法：
-* 双击直接运行
-* cmd环境下，直接键入文件路径
-* Powershell环境下，键入'cmd /c "[文件路径]"'
-
-## 批量重命名文件.py
+## [批量重命名文件.py](/%E6%89%B9%E9%87%8F%E9%87%8D%E5%91%BD%E5%90%8D%E6%96%87%E4%BB%B6.py)
 ### 介绍：
 * 使用Python从Excel中获取学生信息，并对文件进行批量重命名
 ### 环境要求：
@@ -68,6 +27,50 @@
 * openpyxl： pip install openpyxl
 ### 用法：
 * pass
+
+## [下载B站视频弹幕.py](/%E4%B8%8B%E8%BD%BDB%E7%AB%99%E8%A7%86%E9%A2%91%E5%BC%B9%E5%B9%95.py)
+### 介绍：
+* 使用Python获取Bilibili视频的弹幕xml文件，并将其转换为ass文件以便于本地视频播放器使用。
+### 环境要求：
+* 忘了....
+### 环境配置：
+* pass
+### 用法：
+* pass
+
+## [下载B站动漫弹幕.py](/%E4%B8%8B%E8%BD%BDB%E7%AB%99%E5%8A%A8%E6%BC%AB%E5%BC%B9%E5%B9%95.py)
+### 介绍：
+* 使用Python获取Bilibili动漫的弹幕xml文件，并将其转换为ass文件以便于本地视频播放器使用。
+### 环境要求：
+* 忘了....
+### 环境配置：
+* pass
+### 用法：
+* pass
+
+## [PowerfulPixivDownloadHelper.py](/PowerfulPixivDownloadHelper.py)
+### 介绍：
+* 辅助Chrome插件“Powerful Pixiv Downloader”的脚本，将该插件导出的记录按照游戏分类，复制到新的xlsx文件中
+### 环境要求：
+* Python 3 及以上版本
+* openpyxl
+### 环境配置：
+* openpyxl： pip install openpyxl
+### 用法：
+* pass
+
+## [《大学生创新创业》搜题工具.exe（已迁移）](https://github.com/Steven-Zhl/YNU_DaChuang_MOOC)
+### 介绍：
+* 使用pyinstaller将解析题目的爬虫和搜索题目的Python源文件打包生成的文件
+### 环境要求：
+* Windows
+### 环境配置：
+* 无
+### 用法：
+* 需要提前将《大学生创新创业》的考试页面保存到本地
+* 双击直接运行
+* 按提示依次输入考试页面的地址并选择模式
+* 等待输出答案
 
 ## 一键关机.bat
 ### 介绍：
@@ -85,7 +88,7 @@
 
 ## 一键关机.exe
 ### 介绍：
-* 利用C语言可直接执行cmd命令的特点，将一键关机的cmd命令编译之后得到的可执行文件
+* 将上面这个bat使用msvc编译后的结果
 ### 环境要求：
 * Windows
 ### 环境配置：
@@ -93,4 +96,16 @@
 ### 用法：
 * 双击直接运行
 
-## 进制转换.py
+# 电池状态报告.bat
+### 介绍：
+* 使用cmd命令生成电池状态报告文件，并自动打开
+### 环境要求：
+* Windows
+* cmd
+* Powershell
+### 环境配置：
+* 无
+### 用法：
+* 双击直接运行
+* cmd环境下，直接键入文件路径
+* Powershell环境下，键入'cmd /c "[文件路径]"'
